d4004a53cfbc8a88b28236b03df19e99e1ac6230
wonju <jwwon00@naver.com>
2023-09-26 08:32:58 +0900

    drill 4 제출 2019182025_원정우
    drill 4 제출 2019182025_원정우

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character_image = load_image('my_character.png')
 
-# 캐릭터 걷는 동작 프레임 최대한 맞춘 위치
+# 캐릭터 왼쪽,오른쪽 걷는 동작 프레임 최대한 맞춘 위치
 frame_info = [
     {'x_move':86, 'num' : 1,'wide':32},
     {'x_move':119, 'num' : 5,'wide':28},
@@ -14,12 +14,14 @@ frame_info = [
     {'x_move':370, 'num' : 4,'wide':32},
     {'x_move':499, 'num' : 2,'wide':32},
 ]
-
-count_len = len(frame_info)
+# 캐릭터 위,아래 걷는 동작 프레임 최대한 맞춘 위치
+frame_info2 = [
+    {'x_move':113, 'num' : 5,'wide':27},
+]
 
 #방향키 4개와 esc입력 처리
 def handle_events():
-    global running, dix,diy,check
+    global running, dix,diy,check,ycheck
 
     events = get_events()
 
@@ -37,8 +39,10 @@ def handle_events():
                 check = -1
             elif event.key == SDLK_UP:
                 diy += 1
+                ycheck = 1
             elif event.key == SDLK_DOWN:
                 diy -= 1
+                ycheck = 1
             elif event.key == SDLK_ESCAPE:
                 running = False
 
@@ -52,17 +56,20 @@ def handle_events():
                 check = -1
             elif event.key == SDLK_UP:
                 diy -= 1
+                ycheck = 0
             elif event.key == SDLK_DOWN:
                 diy += 1
+                ycheck = 0
 
 # main
 #캐릭터 좌표
-x,y = 400,300
+x,y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 #입력된 키의 이동값
 dix, diy = 0, 0
 #캐릭터 연출을 위한 반복문의 변수
-count, i, check =0, 0, 0
-
+count, i, check= 0, 0,0
+ycheck=0
+ia = 0
 running = True
 
 
@@ -74,19 +81,31 @@ while running:
         i =0
         count = count +1
 
+    if ia==frame_info2[0]['num']:
+        ia=0
+        
     count = count%5
 
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
 #왼쪽을 봤을때 왼쪽 모션
     if dix == -1 or check == -1:
-        character_image.clip_composite_draw(frame_info[count]['x_move']+frame_info[count]['wide']*i,160, frame_info[count]['wide'], 40,0,'h', x, y ,128,128)
+        if ycheck == 1:
+            character_image.clip_composite_draw(frame_info2[0]['x_move']+frame_info2[0]['wide']*ia,0, frame_info2[0]['wide'], 45,0,'h', x, y ,128,128)
+            ia = ia +1
+        else:
+            character_image.clip_composite_draw(frame_info[count]['x_move']+frame_info[count]['wide']*i,160, frame_info[count]['wide'], 40,0,'h', x, y ,128,128)
+            i = i+1
 #오른쪽을 봤을때 오른쪽 모션
     else :
-        character_image.clip_draw(frame_info[count]['x_move']+frame_info[count]['wide']*i,160, frame_info[count]['wide'], 40, x, y ,128,128)
+        if ycheck==1:
+            character_image.clip_draw(frame_info2[0]['x_move']+frame_info2[0]['wide']*ia,0, frame_info2[0]['wide'], 45, x, y ,128,128)
+            ia = ia +1
+        else:
+            character_image.clip_draw(frame_info[count]['x_move']+frame_info[count]['wide']*i,160, frame_info[count]['wide'], 40, x, y ,128,128)
+            i = i+1
 
-    i = i+1
-
+    
     update_canvas()
     handle_events()
 
